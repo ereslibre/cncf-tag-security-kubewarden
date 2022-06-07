6e0d184f1fab3836b1bea29117f54c5e5caca3f8
Rafael Fernández López <ereslibre@ereslibre.es>
2022-06-07 12:40:13 +0300

    Update

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: kwctl
+kwctl:
+	@clear
+	@go run . --kwctl
+
+.PHONY: policy-server
+policy-server:
+	@clear
+	@go run . --policy-server
+
+.PHONY: psp
+psp-disallow-host-network:
+	@clear
+	@go run . --psp-disallow-host-network
+
+.PHONY: gatekeeper
+gatekeeper:
+	@clear
+	@go run . --gatekeeper
---
+++ main.go
@@ -0,0 +1,304 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	demo "github.com/saschagrunert/demo"
+)
+
+func main() {
+	d := demo.New()
+	d.Add(kwctlRun(), "kwctl", "kwctl")
+	d.Add(policyServerRun(), "policy-server", "policy-server")
+	d.Add(gatekeeperPolicyBuildAndRun(), "gatekeeper", "gatekeeper")
+	d.Add(pspDisallowHostNetwork(), "psp-disallow-host-network", "psp-disallow-host-network")
+	d.Run()
+}
+
+func kwctlRun() *demo.Run {
+	r := demo.NewRun(
+		"Running policies with kwctl",
+	)
+
+	r.Setup(cleanupKwctl)
+	r.Cleanup(cleanupKwctl)
+
+	kwctl(r)
+
+	return r
+}
+
+func kwctl(r *demo.Run) {
+	r.Step(demo.S(
+		"List policies",
+	), demo.S("kwctl policies"))
+
+	r.Step(demo.S(
+		"Pull a policy",
+	), demo.S("kwctl pull registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1"))
+
+	r.Step(demo.S(
+		"List policies",
+	), demo.S("kwctl policies"))
+
+	r.Step(demo.S(
+		"Inspect policy",
+	), demo.S("kwctl inspect registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1"))
+
+	r.Step(demo.S(
+		"Request with a letsencrypt-production issuer",
+	), demo.S("bat test_data/production-ingress.json"))
+
+	r.Step(demo.S(
+		"Evaluate request with a letsencrypt-production issuer",
+	), demo.S("kwctl -v run",
+		`--settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}'`,
+		"--request-path test_data/production-ingress.json",
+		"registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1 | jq"))
+
+	r.Step(demo.S(
+		"Request with a letsencrypt-staging issuer",
+	), demo.S("bat test_data/staging-ingress.json"))
+
+	r.StepCanFail(demo.S(
+		"Evaluate request with a letsencrypt-staging issuer",
+	), demo.S("kwctl -v run",
+		`--settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}'`,
+		"--request-path test_data/staging-ingress.json",
+		"registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1 | jq"))
+}
+
+func policyServerRun() *demo.Run {
+	r := demo.NewRun(
+		"Running policies on the policy-server",
+	)
+
+	r.Setup(setupKubernetes)
+	r.Cleanup(cleanupKubernetes)
+
+	policyServer(r)
+
+	return r
+}
+
+func policyServer(r *demo.Run) {
+	r.Step(demo.S(
+		"Pull a policy",
+	), demo.S("kwctl pull registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1"))
+
+	r.Step(demo.S(
+		"Generate Kubernetes manifest",
+	), demo.S("kwctl manifest",
+		"--type ClusterAdmissionPolicy",
+		`--settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}'`,
+		"registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1 | bat --language yaml",
+	))
+
+	r.Step(demo.S(
+		"Apply Kubernetes manifest",
+	), demo.S(
+		"kwctl manifest",
+		"--type ClusterAdmissionPolicy",
+		`--settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}'`,
+		"registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.1 |",
+		"kubectl apply -f -"))
+
+	r.Step(demo.S(
+		"Wait for our policy to be active",
+	), demo.S(
+		"kubectl wait --for=condition=PolicyActive clusteradmissionpolicy generated-policy",
+	))
+
+	r.Step(demo.S(
+		"Ingress with a letsencrypt-production issuer",
+	), demo.S("bat test_data/production-ingress-resource.yaml"))
+
+	r.Step(demo.S(
+		"Deploy an Ingress resource with a letsencrypt-production issuer",
+	), demo.S("kubectl apply -f test_data/production-ingress-resource.yaml"))
+
+	r.Step(demo.S(
+		"Ingress with a letsencrypt-staging issuer",
+	), demo.S("bat test_data/staging-ingress-resource.yaml"))
+
+	r.StepCanFail(demo.S(
+		"Deploy an Ingress resource with a letsencrypt-staging issuer",
+	), demo.S("kubectl apply -f test_data/staging-ingress-resource.yaml"))
+}
+
+func gatekeeperPolicyBuildAndRun() *demo.Run {
+	r := demo.NewRun(
+		"Running a gatekeeper policy",
+	)
+
+	r.Setup(setupKubernetes)
+	r.Cleanup(cleanupKubernetes)
+
+	r.Step(demo.S(
+		"Show policy",
+	), demo.S("bat gatekeeper/requiredlabels.rego"))
+
+	r.Step(demo.S(
+		"Build policy",
+	), demo.S(
+		"opa build -t wasm -e k8srequiredlabels/violation -o gatekeeper/bundle.tar.gz gatekeeper/requiredlabels.rego",
+	))
+
+	r.Step(demo.S(
+		"Extract policy",
+	), demo.S(
+		"tar -C gatekeeper -xf gatekeeper/bundle.tar.gz /policy.wasm",
+	))
+
+	r.Step(demo.S(
+		"Show a request that is valid -- contains an 'owner-team' key",
+	), demo.S(
+		"bat test_data/having-label-deployment.json",
+	))
+
+	r.Step(demo.S(
+		"Run policy with a request that is valid",
+	), demo.S(
+		"kwctl run -e gatekeeper",
+		`--settings-json '{"labels":[{"key":"owner-team"}]}'`,
+		"--request-path test_data/having-label-deployment.json",
+		"gatekeeper/policy.wasm | jq",
+	))
+
+	r.Step(demo.S(
+		"Show a request that is invalid -- does not contain an 'owner-team' key",
+	), demo.S(
+		"bat test_data/missing-label-deployment.json",
+	))
+
+	r.StepCanFail(demo.S(
+		"Run policy with a request that is invalid",
+	), demo.S(
+		"kwctl run -e gatekeeper",
+		`--settings-json '{"labels":[{"key":"owner-team"}]}'`,
+		"--request-path test_data/missing-label-deployment.json",
+		"gatekeeper/policy.wasm | jq",
+	))
+
+	r.Step(demo.S(
+		"Run the policy on top of Kubernetes",
+	), demo.S(
+		"bat kubernetes/required-owner-team.yaml",
+	))
+
+	r.Step(demo.S(
+		"Run the policy on top of Kubernetes",
+	), demo.S(
+		"kubectl apply -f kubernetes/required-owner-team.yaml",
+	))
+
+	r.Step(demo.S(
+		"Run the policy on top of Kubernetes",
+	), demo.S(
+		"kubectl wait --for=condition=PolicyActive clusteradmissionpolicy required-owner-team",
+	))
+
+	r.Step(demo.S(
+		"Run the policy on top of Kubernetes",
+	), demo.S(
+		"kubectl get -o wide clusteradmissionpolicy",
+	))
+
+	r.Step(demo.S(
+		"Show a Deployment with an owner-team label",
+	), demo.S(
+		"bat test_data/having-label-deployment-resource.yaml",
+	))
+
+	r.Step(demo.S(
+		"Create a Deployment with an owner-team label",
+	), demo.S(
+		"kubectl apply -f test_data/having-label-deployment-resource.yaml",
+	))
+
+	r.Step(demo.S(
+		"Show a Deployment without an owner-team label",
+	), demo.S(
+		"bat test_data/missing-label-deployment-resource.yaml",
+	))
+
+	r.StepCanFail(demo.S(
+		"Try to create a Deployment with a missing owner-team label",
+	), demo.S(
+		"kubectl apply -f test_data/missing-label-deployment-resource.yaml",
+	))
+
+	return r
+}
+
+func pspDisallowHostNetwork() *demo.Run {
+	r := demo.NewRun(
+		"Running a Pod Security Policy replacement",
+	)
+
+	r.Setup(setupKubernetes)
+	r.Cleanup(cleanupKubernetes)
+
+	r.Step(demo.S(
+		"Pull a policy",
+	), demo.S("kwctl pull registry://ghcr.io/kubewarden/policies/host-namespaces-psp:v0.1.1"))
+
+	r.Step(demo.S(
+		"Apply Kubernetes manifest",
+	), demo.S(
+		"kubectl apply -f test_data/host-namespaces-policy.yaml"))
+
+	r.Step(demo.S(
+		"Wait for our policy to be active",
+	), demo.S(
+		"kubectl wait --for=condition=PolicyActive clusteradmissionpolicy disallow-host-network-except-kube-system",
+	))
+
+	r.Step(demo.S(
+		"Show a pod with hostNetwork: true in the default namespace",
+	), demo.S(
+		"bat test_data/host-network-pod.yaml",
+	))
+
+	r.StepCanFail(demo.S(
+		"Try to create a pod with hostNetwork: true in the default namespace",
+	), demo.S(
+		"kubectl apply -f test_data/host-network-pod.yaml",
+	))
+
+	r.Step(demo.S(
+		"Show a pod with hostNetwork: true in the kube-system namespace",
+	), demo.S(
+		"bat test_data/host-network-pod-kube-system.yaml",
+	))
+
+	r.Step(demo.S(
+		"Try to create a pod with hostNetwork: true in the kube-system namespace",
+	), demo.S(
+		"kubectl apply -f test_data/host-network-pod-kube-system.yaml",
+	))
+
+	return r
+}
+
+func cleanupKwctl() error {
+	os.RemoveAll(filepath.Join(os.Getenv("HOME"), ".cache", "kubewarden"))
+	return nil
+}
+
+func setupKubernetes() error {
+	cleanupKwctl()
+	cleanupKubernetes()
+	exec.Command("kubectl", "create", "namespace", "rancher-online-meetup-21").Run()
+	exec.Command("kubectl", "delete", "clusteradmissionpolicy", "--all").Run()
+	return nil
+}
+
+func cleanupKubernetes() error {
+	cleanupKwctl()
+	exec.Command("kubectl", "delete", "namespace", "rancher-online-meetup-21").Run()
+	exec.Command("kubectl", "delete", "clusteradmissionpolicy", "--all").Run()
+	return nil
+}
